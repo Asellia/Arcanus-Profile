88d56e382501656a481307b3b52620537d7fe4fc
Asellia <recon01970@yahoo.com>
2021-10-11 18:34:25 -0400

    Update arcanus.js

--- Arcanus.js
+++ Arcanus.js
@@ -1,5 +1,6 @@
 const sidebar_links = document.querySelectorAll(".links .link");
-
+document.querySelector(".sidebar").classList.toggle("active");
+document.querySelector(".mainpanel").classList.toggle("active");
 document.querySelector(".sidebar .toggle-btn").addEventListener("click",function(){
 	document.querySelector(".sidebar").classList.toggle("active");
   document.querySelector(".mainpanel").classList.toggle("active");
